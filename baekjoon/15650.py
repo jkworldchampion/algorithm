64ce29702665bfaee76d64c611f375f8c712f200
JuHwan_Park <jkworldchampion@ajou.ac.kr>
2024-01-31 23:42:33 +0900

    Create 15650.py

---
+++ baekjoon/15650.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+s = list()
+def bfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))   # 여기서 map 사용 주의
+    
+    for i in range(1, n+1):
+        if i not in s:
+            if len(s) == 0:
+                s.append(i)
+                bfs()
+                s.pop()
+            else: 
+                if (i > (s[len(s)-1])): 
+                    s.append(i)
+                    bfs()
+                    s.pop()
+bfs()
